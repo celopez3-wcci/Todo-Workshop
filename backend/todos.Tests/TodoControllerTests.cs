03858002031478146f6f144aee80c65c8abef522
Carlos Lopez <=>
2021-03-26 14:04:18 -0400

    added Tests

---
+++ backend/todos.Tests/TodoControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using todos.Controllers;
+using todos.Models;
+using todos.Repositories;
+using Xunit;
+
+namespace todos.Tests
+{
+    public class TodoControllerTests
+    {
+        TodoController sut;
+        IRepository<Todo> todoRepo;
+
+        public TodoControllerTests()
+        {
+            todoRepo = Substitute.For<IRepository<Todo>>();
+            sut = new TodoController(todoRepo);
+        }
+
+        [Fact]
+        public void GetTodos_Returns_A_List()
+        {
+            // Arrange
+            IEnumerable<Todo> expectedList = new List<Todo>();
+            todoRepo.GetAll().Returns(expectedList);
+
+            // Act
+            var result = sut.GetTodos();
+
+            //Assert 
+            Assert.Equal(expectedList, result);
+
+        }
+
+        [Fact]
+        public void GetTodo_Returns_A_Todo()
+        {
+            // Arrange
+            var expectedTodo = new Todo();
+            todoRepo.GetById(1).Returns(expectedTodo);
+
+            // Assert
+            var result = sut.GetTodo(1);
+
+            // Act
+            Assert.Equal(expectedTodo, result);
+        }
+
+    }
+}
---
+++ backend/todos.Tests/TodoTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using todos.Models;
+using Xunit;
+
+namespace todos.Tests
+{
+    public class TodoTest
+    {
+        Todo sut;
+
+        public TodoTest()
+        {
+            sut = new Todo();
+            sut.Id = 1;
+            sut.Name = "";
+            sut.CreatedOn = DateTime.Now;
+        }
+
+        [Fact]
+        public void Todo_Sets_Name_On_TodoModel()
+        {
+            // Arrange
+            // Act
+            string name = sut.Name;
+            // Assert
+
+            Assert.Equal("", name);
+        }
+
+    }
+}
